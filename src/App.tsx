2861edf6088a8f298a1f5183b5bf181192fd93a5
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2025-03-20 19:10:00 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import TheWrapper from '@/containers/TheWrapper';
+import Header from '@/containers/TheHeader';
+import Footer from '@/containers/TheFooter';
+
+function App() {
+ return (
+  <>
+   <TheWrapper>
+    <Header />
+    <main>test</main>
+    <Footer />
+   </TheWrapper>
+  </>
+ );
+}
+
+export default App;
---
+++ src/core/store/pattern.ts
@@ -0,0 +1,9 @@
+// import { create } from 'zustand';
+// import { MyCounter } from '@/core/types/';
+
+// const useStore = create<MyCounter>((set) => ({
+//  counter: 0,
+//  increment: () => set((state: any) => ({ counter: state.counter + 1 })),
+// }));
+
+// export default useStore;
